33b94bfe48265f0114631fe685b61fca82f3d274
jadonvs <vijay.jadon1987@gmail.com>
2020-01-29 11:33:58 +0530

    first commit

---
+++ src/test/java/WebApplicationAutomation.java
@@ -0,0 +1,13 @@
+
+public class WebApplicationAutomation {
+
+	public void javaScriptBasedApp()
+	{
+		System.out.println("This is java script app");
+	}
+	
+	public void angularBasedApp()
+	{
+		System.out.println("This is angular based app");
+	}
+}
