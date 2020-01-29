22807191204b72985f0995e935461643ca61e035
jadonvs <vijay.jadon1987@gmail.com>
2020-01-29 11:58:11 +0530

    Second Commit

--- src/test/java/APITestAutomaiton.java
+++ src/test/java/APITestAutomaiton.java
@@ -1,4 +1,14 @@
 
 public class APITestAutomaiton {
+	
+	public void RESTAPItest()
+	{
+		System.out.println("This is for API Testing");
+	}
+	
+	public void SoapUItest()
+	{
+		System.out.println("This is for SoapUI Testing");
+	}
 
 }
